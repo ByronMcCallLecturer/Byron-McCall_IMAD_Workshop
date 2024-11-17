37b64a11a74c2bd7f7c0b074436b274b30340a60
ByronMcCallLecturer <bmcall@varsitycollege.co.za>
2024-11-17 18:38:18 +0200

    Finished Project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,11 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+        viewBinding = true
     }
 
     buildTypes {
@@ -26,10 +29,12 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
